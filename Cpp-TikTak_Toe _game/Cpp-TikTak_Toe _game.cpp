34745b13e8e3f54290b2c31a87f1b24d33973c89
Borislav Lukanov <borislav.lukanov@outlook.com>
2021-08-11 16:29:03 +0300

    v 1.0 :D

--- Cpp-TikTak_Toe _game/Cpp-TikTak_Toe _game.cpp
+++ Cpp-TikTak_Toe _game/Cpp-TikTak_Toe _game.cpp
@@ -1,20 +1,97 @@
-// Cpp-TikTak_Toe _game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 
-int main()
+char matrix[3][3];
+char check(void);
+void init_matrix(void);
+void get_player_move(void);
+void get_computer_move(void);
+void disp_matrix(void);
+
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    char done;
+    printf("This is the game of Tic Tac Toe.\n");
+    printf("You will be playing against the computer.\n");
+    done = ' ';
+
+    init_matrix();
+
+    do 
+    {
+        disp_matrix();
+        get_player_move();
+        done = check();
+        if (done != ' ') break;
+        get_computer_move();
+        done = check();
+    } while (done == ' ');
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void init_matrix(void)
+{
+    int i, j;
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            matrix[i][j] = ' ';
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void get_player_move(void)
+{
+    int x, y;
+    printf("Enter X,Y coordinates for your move: ");
+    scanf_s("%d%*c%d", &x, &y);
+    x--; y--;
+    if (matrix[x][y] != ' ')
+    {
+        printf("Invalid move, try again.\n");
+        get_player_move();
+    }
+    else matrix[x][y] = 'X';
+}
+
+void get_computer_move(void)
+{
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+            if (matrix[i][j] == ' ')break;
+        if (matrix[i][j] == ' ') break;
+    }
+    if (i * j == 9)
+    {
+        printf("draw\n");
+    }
+    else
+        matrix[i][j] = 'O';
+}
+
+void disp_matrix(void)
+{
+    int t;
+    for (t = 0; t < 3; t++)
+    {
+        printf(" %c | %c | %c", matrix[t][0], matrix[t][1], matrix[t][2]);
+        if (t != 2) printf("\n---|---|---\n");
+    }
+    printf("\n");
+}
+
+char check(void)
+{
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2]) return matrix[i][0];
+    }
+    for (i = 0; i < 3; i++)
+    {
+        if (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i]) return matrix[0][i];
+    }
+    if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) return matrix[0][0];
+    if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) return matrix[0][2];
+    return' ';
+}
